6a25c22ee3cf7261d23613a7d79f5ba7872f5f4e
kauanr0d <kauankenney16752@gmail.com>
2023-10-14 15:27:16 -0300

    classe run

--- src/Run/Run.java
+++ src/Run/Run.java
@@ -1,13 +1,37 @@
 package Run;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import util.TabelaHash;
 
 public class Run {
 
     public static void main(String[] args) {
-        TabelaHash t = new TabelaHash(1);
-        t.inserir(41, "12");
-        t.imprimir();
+        String caminho = "./src/entrada/executarQuestao.txt";
 
+        try (BufferedReader br = new BufferedReader(new FileReader(caminho))) {
+            String questao = br.readLine();
+            switch (questao) {
+                case "questao2":
+                    questao2.Main.main(args);
+                    break;
+                case "questao3":
+                    questao3.Main.main(args);
+                    break;
+                default:
+                    System.out.println("Questão inexistente");
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Run.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Run.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            System.out.printf("%s\n", e);
+        }
     }
+
 }
